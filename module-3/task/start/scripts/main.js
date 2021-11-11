21147745ab2bd5625a40a458d1eafe560db02173
FaiqaDevel0per <86679606+FaiqaDevel0per@users.noreply.github.com>
2021-11-11 21:15:57 +0000

    adding javascript

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,22 @@
+console.log("Hello")
+
+// Single line comment
+
+/*-------------
+
+
+Multiple line comment
+
+---------------*/
+
+let productName = 'alex';
+let price = '10.00'
+let quantity = 3;
+let Instock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(Instock);
+console.log(discountAmount);
