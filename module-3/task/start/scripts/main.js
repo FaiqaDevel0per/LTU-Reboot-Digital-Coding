f6551b546ed2ae152288cfdcbfc8c63c5ae14873
FaiqaDevel0per <86679606+FaiqaDevel0per@users.noreply.github.com>
2021-11-16 21:03:32 +0000

    JavaScript
    Completing tasks from JavaScript tasks

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -9,14 +9,68 @@ Multiple line comment
 
 ---------------*/
 
-let productName = 'alex';
+let productName = 'Faiqa';
 let price = '10.00'
 let quantity = 3;
 let Instock = true;
-let discountAmount = 0;
+let discountAmount = 2;
 
 console.log(productName);
 console.log(price);
 console.log(quantity);
 console.log(Instock);
 console.log(discountAmount);
+
+// declaring the function here
+
+function showProductName(){
+    console.log(productName);
+}
+
+showProductName();
+
+function totalPrice(productPrice=0,productQuantity=0){
+    productPrice = parseFloat(productPrice);
+    productQuantity = parseFloat(productQuantity);
+    let totalProductPrice = productPrice * productQuantity;
+    console.log(totalProductPrice);
+}
+
+totalPrice(price,quantity);
+
+function squareNumber(number=0){
+    let sum = number * number;
+    return number;
+}
+
+var square = num => num * num;
+
+let squaredNumber = squareNumber(3);
+console.log(squaredNumber);
+
+let squared = square(3);
+console.log(squared);
+//showing the difference between a regular function and an arrow function
+
+function productDiscount(){
+    let sum;
+    if(quantity > 1){
+        sum = price * quantity;
+        let discount = discountAmount * quantity;
+        sum = sum - discount;
+    } else {
+        console.log(sum)
+    }
+}
+productDiscount();
+
+//function calculateDiscount(){
+    //let sum = price * quantity;
+    //if(quantity > 1){
+      //  let discount = discountAmount * quantity;
+       // sum = sum - discount;
+    //}
+    //console.log(sum);
+//}
+
+//calculateDiscount();
